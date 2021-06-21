41cfeaa213d10bb7c17fe170ae55106e8080876c
serhatkg021 <serhatkg.2654@gmail.com>
2021-06-21 05:16:03 +0300

    Bildirim kontrol servisi ve UnitTesti yazıldı(Rezervasyonu olan uçaklara oluşturulan bildirimler).

--- MicroServices/BildirimServices.cs
+++ MicroServices/BildirimServices.cs
@@ -37,6 +37,39 @@ namespace MicroServices
             baglanti.Close();
             return donus;
         }
+        public List<Bildirim> bildirimKullaniciKontrol(Kullanici k)
+        {
+            string sorguString = "SELECT b.bildirimlerID, u.ucuslarID, u.havayoluAdi, " +
+                "u.kalkisYeri, u.inisYeri, u.seferTarihi, b.bildirimIcerigi, b.aktiflik  " +
+                "FROM bildirimler as b " +
+                "LEFT JOIN ucuslar as u ON b.ucusID = u.ucuslarID " +
+                "LEFT JOIN kullaniciRezervasyon as kr ON b.ucusID = kr.ucusID " +
+                "WHERE kr.kullaniciID=@kID";
+            SqlCommand sorgu = new SqlCommand(sorguString, baglanti);
+            sorgu.Parameters.AddWithValue("@kID", k.id);
+            baglanti.Open();
+            SqlDataReader cikti = sorgu.ExecuteReader(); // İşlemdemden etkilenen kayıtları getirir
+            List<Bildirim> bildirimler = new List<Bildirim>();
+
+            while (cikti.Read())
+            {
+                Ucus u = new Ucus();
+                u.id = Convert.ToInt32(cikti["ucuslarID"]);
+                u.havayoluAdi = Convert.ToString(cikti["havayoluAdi"]);
+                u.kalkisYeri = Convert.ToString(cikti["kalkisYeri"]);
+                u.inisYeri = Convert.ToString(cikti["inisYeri"]);
+                u.seferTarih = Convert.ToDateTime(cikti["seferTarihi"]);
+                Bildirim bildirim = new Bildirim();
+                bildirim.bildirimlerID = Convert.ToInt32(cikti["bildirimlerID"]);
+                bildirim.ucus = u;
+                bildirim.bildirimIcerigi = Convert.ToString(cikti["bildirimIcerigi"]);
+                bildirim.durum = Convert.ToBoolean(cikti["aktiflik"]);
+                bildirimler.Add(bildirim);
+            }
+            baglanti.Close();
+            return bildirimler;
+        }
+
 
     }
 }
--- TestKullaniciServices/TestBildirimServices.cs
+++ TestKullaniciServices/TestBildirimServices.cs
@@ -2,6 +2,7 @@
 using System;
 using Entities;
 using MicroServices;
+using System.Collections.Generic;
 
 namespace TestServices
 {
@@ -14,13 +15,30 @@ namespace TestServices
         {
             Bildirim denemeBildirim = new Bildirim();
             Ucus denemeUcus = new Ucus();
-            denemeUcus.id = 1;
+            denemeUcus.id = 2;
             denemeBildirim.ucus = denemeUcus;
             denemeBildirim.bildirimIcerigi = "Uçak rötar dolasıyla 15 dk geç kalkacaktır.";
             denemeBildirim.durum = true;
 
             bool donus = servis.bildirimEkle(denemeBildirim);
             Assert.AreEqual(donus, true);
+        }
+
+        [TestMethod]
+        public void bildirimKullaniciKontrol()
+        {
+            Kullanici denemeKullanici = new Kullanici();
+            denemeKullanici.id = 1;
+            List<Bildirim> bildirimler = new List<Bildirim>();
+
+            bildirimler = servis.bildirimKullaniciKontrol(denemeKullanici);
+
+            bool sonuc = false;
+            if (bildirimler.Count != 0)
+            {
+                sonuc = true;
+            }
+            Assert.AreEqual(sonuc, true);
         }
     }
 }
