3c87fa58d1d038862887babba96c34cbb1a07611
Büşra Güney <Pc@DESKTOP-ND563VH>
2021-06-21 03:12:45 +0300

    Kullanıcı kayıt ekranı yapıldı.

---
+++ bsm208donemprojesigrup5/frmKullaniciKayitEkrani.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace bsm208donemprojesigrup5
+{
+    partial class frmKullaniciKayitEkrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmKullaniciKayitEkrani));
+            this.tbKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.tbAdSoyad = new System.Windows.Forms.TextBox();
+            this.tbSifre = new System.Windows.Forms.TextBox();
+            this.tbSifreTekrar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.dtmDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.lblKullaniciAdiUyari = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbKullaniciAdi
+            // 
+            this.tbKullaniciAdi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tbKullaniciAdi.Location = new System.Drawing.Point(390, 138);
+            this.tbKullaniciAdi.Name = "tbKullaniciAdi";
+            this.tbKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.tbKullaniciAdi.TabIndex = 2;
+            // 
+            // tbAdSoyad
+            // 
+            this.tbAdSoyad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tbAdSoyad.Location = new System.Drawing.Point(390, 43);
+            this.tbAdSoyad.Name = "tbAdSoyad";
+            this.tbAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.tbAdSoyad.TabIndex = 3;
+            // 
+            // tbSifre
+            // 
+            this.tbSifre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tbSifre.Location = new System.Drawing.Point(390, 188);
+            this.tbSifre.Name = "tbSifre";
+            this.tbSifre.Size = new System.Drawing.Size(100, 20);
+            this.tbSifre.TabIndex = 4;
+            // 
+            // tbSifreTekrar
+            // 
+            this.tbSifreTekrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tbSifreTekrar.Location = new System.Drawing.Point(390, 233);
+            this.tbSifreTekrar.Name = "tbSifreTekrar";
+            this.tbSifreTekrar.Size = new System.Drawing.Size(100, 20);
+            this.tbSifreTekrar.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(12, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Ad Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(12, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Şifre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(12, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Kullanıcı Adı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Doğum Tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(12, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Şifre Tekrar";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnKaydet.Location = new System.Drawing.Point(105, 320);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(385, 56);
+            this.btnKaydet.TabIndex = 12;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            // 
+            // dtmDogumTarihi
+            // 
+            this.dtmDogumTarihi.CalendarForeColor = System.Drawing.Color.Black;
+            this.dtmDogumTarihi.CalendarMonthBackground = System.Drawing.Color.White;
+            this.dtmDogumTarihi.CalendarTitleBackColor = System.Drawing.Color.White;
+            this.dtmDogumTarihi.Location = new System.Drawing.Point(390, 90);
+            this.dtmDogumTarihi.Name = "dtmDogumTarihi";
+            this.dtmDogumTarihi.Size = new System.Drawing.Size(200, 20);
+            this.dtmDogumTarihi.TabIndex = 13;
+            // 
+            // lblKullaniciAdiUyari
+            // 
+            this.lblKullaniciAdiUyari.AutoSize = true;
+            this.lblKullaniciAdiUyari.Location = new System.Drawing.Point(396, 161);
+            this.lblKullaniciAdiUyari.Name = "lblKullaniciAdiUyari";
+            this.lblKullaniciAdiUyari.Size = new System.Drawing.Size(0, 13);
+            this.lblKullaniciAdiUyari.TabIndex = 14;
+            // 
+            // frmKullaniciKayitEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(590, 485);
+            this.Controls.Add(this.lblKullaniciAdiUyari);
+            this.Controls.Add(this.dtmDogumTarihi);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbSifreTekrar);
+            this.Controls.Add(this.tbSifre);
+            this.Controls.Add(this.tbAdSoyad);
+            this.Controls.Add(this.tbKullaniciAdi);
+            this.Name = "frmKullaniciKayitEkrani";
+            this.Text = "frmKullaniciKayitEkrani";
+            this.Load += new System.EventHandler(this.frmKullaniciKayitEkrani_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbKullaniciAdi;
+        private System.Windows.Forms.TextBox tbAdSoyad;
+        private System.Windows.Forms.TextBox tbSifre;
+        private System.Windows.Forms.TextBox tbSifreTekrar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.DateTimePicker dtmDogumTarihi;
+        private System.Windows.Forms.Label lblKullaniciAdiUyari;
+    }
+}
---
+++ bsm208donemprojesigrup5/frmKullaniciKayitEkrani.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bsm208donemprojesigrup5
+{
+    public partial class frmKullaniciKayitEkrani : Form
+    {
+        public frmKullaniciKayitEkrani()
+        {
+            InitializeComponent();
+        }
+
+        private void frmKullaniciKayitEkrani_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
