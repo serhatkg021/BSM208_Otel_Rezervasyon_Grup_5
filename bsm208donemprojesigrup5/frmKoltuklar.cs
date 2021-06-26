710d0d1cded7cdfcf0e541b1189430fbeeb1f3d7
Zeynep Cansu Yalçın <19010310004@ogrenci.bartin.edu.tr>
2021-06-26 03:25:27 +0300

    Revert "Yöneticinin bildirim ekleme ve kullanıcıların rezervasyonlarını görüntüleme kısmı çalışır hale geldi."
    This reverts commit 18b73a4a42e9715735da371ef27a9030b465170d.

--- bsm208donemprojesigrup5/frmKoltuklar.cs
+++ bsm208donemprojesigrup5/frmKoltuklar.cs
@@ -7,32 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Entities;
-using MicroServices;
 
 namespace bsm208donemprojesigrup5
 {
     public partial class frmKoltuklar : Form
     {
-        RezervasyonServices servis = new RezervasyonServices();
-        Kullanici sistemKullanici = new Kullanici();
-        Ucus secilenUcus = new Ucus();
-
-        public frmKoltuklar(Kullanici k = null,Ucus u = null)
+        public frmKoltuklar()
         {
             InitializeComponent();
-            sistemKullanici = k;
-            secilenUcus = u;
-        }
-
-        private void frmKoltuklar_Load(object sender, EventArgs e)
-        {
-            if (sistemKullanici == null)
-            {
-                btnKoltuk1.BackColor = Color.Red;
-                btnRezervasyonuTamamla.Enabled = false;
-                cbİstenenKoltuguSecin.Enabled = false;
-            }
         }
     }
 }
--- bsm208donemprojesigrup5/frmYoneticiPaneliEkrani.cs
+++ bsm208donemprojesigrup5/frmYoneticiPaneliEkrani.cs
@@ -7,91 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Entities;
-using MicroServices;
 
 namespace bsm208donemprojesigrup5
 {
     public partial class frmYoneticiPaneliEkrani : Form
     {
-        UcusServices servisU = new UcusServices();
-        RezervasyonServices servisR = new RezervasyonServices();
-        BildirimServices servisB = new BildirimServices();
-
-        Kullanici sistemKullanici = new Kullanici();
-        DataTable tabloU = new DataTable();
-        DataTable tabloR = new DataTable();
-        public frmYoneticiPaneliEkrani(Kullanici k = null)
+        public frmYoneticiPaneliEkrani()
         {
             InitializeComponent();
-            sistemKullanici = k;
-        }
-
-        private void frmYoneticiPaneliEkrani_Load(object sender, EventArgs e)
-        {
-            if (sistemKullanici != null)
-            {
-                tabloU.Columns.Add("Uçuş ID", typeof(int));
-                tabloU.Columns.Add("Kalkış Yeri", typeof(string));
-                tabloU.Columns.Add("İniş Yeri", typeof(string));
-                tabloU.Columns.Add("Havayolu Adı", typeof(string));
-                tabloU.Columns.Add("Sefer Tarihi", typeof(DateTime));
-                List<Ucus> ucuslar = servisU.ucusListeYonetici();
-                for (int i = 0; i < ucuslar.Count; i++)
-                {
-                    tabloU.Rows.Add(ucuslar[i].id,
-                                   ucuslar[i].kalkisYeri,
-                                   ucuslar[i].inisYeri,
-                                   ucuslar[i].havayoluAdi,
-                                   ucuslar[i].seferTarih);
-                }
-                dgvYoneticiUcuslar.DataSource = tabloU;
-
-                tabloR.Columns.Add("Rezervasyon ID", typeof(int));
-                tabloR.Columns.Add("Kullanıcı ID", typeof(int));
-                tabloR.Columns.Add("Adı Soyadı", typeof(string));
-                tabloR.Columns.Add("Uçuş ID", typeof(int));
-                tabloR.Columns.Add("Acenta Adı", typeof(string));
-                tabloR.Columns.Add("Koltuk No", typeof(int));
-                tabloR.Columns.Add("Bilet Fiyatı", typeof(decimal));
-                tabloR.Columns.Add("Rezervasyon Tarihi", typeof(DateTime));
-                List<Rezervasyon> rezervasyonlar = servisR.yoneticiRezervasyonListe();
-                for (int i = 0; i < rezervasyonlar.Count; i++)
-                {
-                    tabloR.Rows.Add(rezervasyonlar[i].id,
-                                    rezervasyonlar[i].kullanici.id, 
-                                    rezervasyonlar[i].kullanici.kullaniciAdiSoyadi, 
-                                    rezervasyonlar[i].ucus.id,
-                                    rezervasyonlar[i].acentaAdi,
-                                    rezervasyonlar[i].koltukNo,
-                                    rezervasyonlar[i].biletFiyat,
-                                    rezervasyonlar[i].olusturmaTarih);
-                }
-                dgvYoneticiRezervasyonlar.DataSource = tabloR;
-            }
-        }
-
-        private void dgvYoneticiUcuslar_MouseClick(object sender, MouseEventArgs e)
-        {
-            tbSecilenUcusID.Text = dgvYoneticiUcuslar.SelectedRows[0].Cells["Uçuş ID"].Value.ToString();
-        }
-
-        private void btnBildirimiGonder_Click(object sender, EventArgs e)
-        {
-            Bildirim bildirim = new Bildirim();
-            Ucus ucus = new Ucus();
-            ucus.id = Convert.ToInt32(tbSecilenUcusID.Text);
-            bildirim.ucus = ucus;
-            bildirim.bildirimIcerigi = tbBildirimİcerigi.Text;
-            bildirim.durum = true;
-            if (servisB.bildirimEkle(bildirim))
-            {
-                MessageBox.Show("Bildirim Eklenmiştir.");
-            }
-            else
-            {
-                MessageBox.Show("Bildirim Eklenilirken Bir Hata Oluştu.");
-            }
         }
     }
 }
