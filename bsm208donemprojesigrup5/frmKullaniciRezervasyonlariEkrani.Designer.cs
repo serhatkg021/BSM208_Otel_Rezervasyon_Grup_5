e5ba86eeac60ee88d58b4c331a071b96ecaf716b
Zeynep Cansu Yalçın <19010310004@ogrenci.bartin.edu.tr>
2021-06-26 03:20:12 +0300

    Kullanıcı giriş yaptıysa kendi rezervasyonlarını gördüğü ekrana kullanıcı bazlı rezervasyonları getiren servis entegre edildi.

--- bsm208donemprojesigrup5/frmKullaniciRezervasyonlariEkrani.Designer.cs
+++ bsm208donemprojesigrup5/frmKullaniciRezervasyonlariEkrani.Designer.cs
@@ -39,30 +39,36 @@ namespace bsm208donemprojesigrup5
             // 
             this.gbRezervasyonlar.Controls.Add(this.dgvKullaniciRezervasyonlar);
             this.gbRezervasyonlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.gbRezervasyonlar.Location = new System.Drawing.Point(28, 12);
+            this.gbRezervasyonlar.Location = new System.Drawing.Point(37, 15);
+            this.gbRezervasyonlar.Margin = new System.Windows.Forms.Padding(4);
             this.gbRezervasyonlar.Name = "gbRezervasyonlar";
-            this.gbRezervasyonlar.Size = new System.Drawing.Size(562, 278);
+            this.gbRezervasyonlar.Padding = new System.Windows.Forms.Padding(4);
+            this.gbRezervasyonlar.Size = new System.Drawing.Size(1043, 366);
             this.gbRezervasyonlar.TabIndex = 0;
             this.gbRezervasyonlar.TabStop = false;
             this.gbRezervasyonlar.Text = "Rezervasyonlarım";
             // 
             // dgvKullaniciRezervasyonlar
             // 
+            this.dgvKullaniciRezervasyonlar.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvKullaniciRezervasyonlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvKullaniciRezervasyonlar.Location = new System.Drawing.Point(6, 23);
+            this.dgvKullaniciRezervasyonlar.Location = new System.Drawing.Point(8, 28);
+            this.dgvKullaniciRezervasyonlar.Margin = new System.Windows.Forms.Padding(4);
             this.dgvKullaniciRezervasyonlar.Name = "dgvKullaniciRezervasyonlar";
-            this.dgvKullaniciRezervasyonlar.Size = new System.Drawing.Size(550, 249);
+            this.dgvKullaniciRezervasyonlar.RowHeadersWidth = 51;
+            this.dgvKullaniciRezervasyonlar.Size = new System.Drawing.Size(1027, 330);
             this.dgvKullaniciRezervasyonlar.TabIndex = 0;
-            this.dgvKullaniciRezervasyonlar.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // frmKullaniciRezervasyonlariEkrani
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(606, 300);
+            this.ClientSize = new System.Drawing.Size(1122, 394);
             this.Controls.Add(this.gbRezervasyonlar);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmKullaniciRezervasyonlariEkrani";
             this.Text = "Kullanıcı Rezervasyonları Ekranı";
+            this.Load += new System.EventHandler(this.frmKullaniciRezervasyonlariEkrani_Load);
             this.gbRezervasyonlar.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvKullaniciRezervasyonlar)).EndInit();
             this.ResumeLayout(false);
--- bsm208donemprojesigrup5/frmKullaniciRezervasyonlariEkrani.cs
+++ bsm208donemprojesigrup5/frmKullaniciRezervasyonlariEkrani.cs
@@ -7,19 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Entities;
+using MicroServices;
 
 namespace bsm208donemprojesigrup5
 {
     public partial class frmKullaniciRezervasyonlariEkrani : Form
     {
-        public frmKullaniciRezervasyonlariEkrani()
+        RezervasyonServices servis = new RezervasyonServices();
+        Kullanici sistemKullanici = new Kullanici();
+        DataTable tablo = new DataTable();
+        public frmKullaniciRezervasyonlariEkrani(Kullanici k = null)
         {
             InitializeComponent();
+            sistemKullanici = k;
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void frmKullaniciRezervasyonlariEkrani_Load(object sender, EventArgs e)
         {
-
+            if (sistemKullanici != null)
+            {
+                tablo.Columns.Add("Uçuş ID", typeof(int));
+                tablo.Columns.Add("Acenta Adı", typeof(string));
+                tablo.Columns.Add("Kalkış Yeri", typeof(string));
+                tablo.Columns.Add("İniş Yeri", typeof(string));
+                tablo.Columns.Add("Havayolu Adı", typeof(string));
+                tablo.Columns.Add("Sefer Tarihi", typeof(DateTime));
+                tablo.Columns.Add("Bilet Fiyatı", typeof(decimal));
+                tablo.Columns.Add("Rezervasyon Tarihi", typeof(DateTime));
+                List<Rezervasyon> rezervasyonlar = servis.kullaniciRezervasyonListe(sistemKullanici);
+                for (int i = 0; i < rezervasyonlar.Count; i++)
+                {
+                    tablo.Rows.Add(rezervasyonlar[i].ucus.id, 
+                                   rezervasyonlar[i].acentaAdi,
+                                   rezervasyonlar[i].ucus.kalkisYeri, 
+                                   rezervasyonlar[i].ucus.inisYeri,
+                                   rezervasyonlar[i].ucus.havayoluAdi,
+                                   rezervasyonlar[i].ucus.seferTarih,
+                                   rezervasyonlar[i].biletFiyat,
+                                   rezervasyonlar[i].olusturmaTarih);
+                }
+                dgvKullaniciRezervasyonlar.DataSource = tablo; 
+            }
         }
     }
 }
